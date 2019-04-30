5c522b5ca1406176cf34a03d2c3b47b9a4f475e0
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-30 16:56:41 -0700

    join table between category and product

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_234556) do
+ActiveRecord::Schema.define(version: 2019_04_30_235328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories_products_joins", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "product_id"
+    t.index ["category_id"], name: "index_categories_products_joins_on_category_id"
+    t.index ["product_id"], name: "index_categories_products_joins_on_product_id"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string "username"
