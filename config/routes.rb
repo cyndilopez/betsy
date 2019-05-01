c975690049141bad5676c1cdf2b15313b25d2f6f
Jansen <v.jansen.martin@gmail.com>
2019-05-01 16:45:24 -0700

    Add route that sends merchants to GitHub

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#root"
   resources :products, only: [:index, :show, :new, :create, :edit, :update]
+
+  get "/auth/github", as: "github_login"
 end
