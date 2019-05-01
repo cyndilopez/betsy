01de0340b3fb090c71a9170d551c42494631dee7
Jansen <v.jansen.martin@gmail.com>
2019-05-01 11:21:36 -0700

    Create new and create actions inside Products Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,22 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+
+    successful = @product.save
+    if successful
+      flash[:status] = :success
+      flash[:message] = "Successfully saved #{@product.name} to the database."
+      redirect_to_products_path
+    else
+      flash.now[:status] = :error
+      flash.now[:message] = "Could not save product!"
+      render :new, status: :bad_request
+    end
   end
 
   def edit
