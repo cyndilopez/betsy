4364a6e3e40871986ad5ef874e9eba859a6ed5b9
Maria Wissler <wmcarmelina@gmail.com>
2019-05-01 16:54:32 -0700

    added routes for order_items, create action skeletons for order_ide, added methos to find current order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def current_order
+    if session[:order_id]
+      Order.find_by(id: session[:order_id])
+    else
+      Order.new
+    end
+  end
 end
