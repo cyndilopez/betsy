61fbd2ae898f82b88f5f1a34a038a735164c119c
Jansen <v.jansen.martin@gmail.com>
2019-05-01 16:49:05 -0700

    Add route that GitHub points back to

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show, :new, :create, :edit, :update]
 
   get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "merchants#create"
 end
