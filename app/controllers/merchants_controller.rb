c28d9cb1e08973ff82af117645526cbbe38bf47a
Jansen <v.jansen.martin@gmail.com>
2019-05-02 10:29:58 -0700

    Add conditionals to merchants#create checking for existing users

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -9,7 +9,15 @@ class MerchantsController < ApplicationController
       flash[:message] = "Logged in as returning merchant #{merchant.name}"
     else
       # Merchant doesn't match anything in the DB
-      # TODO: Try to create a new merchant
+      merchant = Merchant.build_from_github(auth_hash)
+      if merchant.save
+        flash[:status] = :success
+        flash[:message] = "Logged in as new merchant #{merchant.name}"
+      else
+        flash[:status] = :error
+        flash[:message] = "Could not create new merchant account: #{merchant.errors.messages}"
+        return redirect_to root_path
+      end
     end
 
     session[:merchant_id] = merchant.id
