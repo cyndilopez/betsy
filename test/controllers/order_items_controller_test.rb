56ae337e3d9d0793c35867da600a274f727e81b2
Amy Phung <phung.amy85@gmail.com>
2019-05-02 15:16:36 -0700

    add create OrderItem method, updated current_order helper method

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe OrderItemsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  before do
+    @product = Product.first
+  end
+  describe "create" do
+  
+    it "can create a new order item" do
+      
+      order_item_data = {
+        order_item: {
+          product_id: @product.id,
+          quantity: 2, 
+        }
+      }
+      
+      expect {
+        post order_items_path, params: order_item_data
+      }.must_change "OrderItem.count", 1
+      
+      
+      expect(session[:order_id]).wont_be_nil
+      
+    end
+  end
 end
