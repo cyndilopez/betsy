f22957cf1ef42292633d29dc847505d0d0a6421d
Amy Phung <phung.amy85@gmail.com>
2019-05-02 10:26:45 -0700

    add create method OrderItems controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,5 +1,16 @@
 class OrderItemsController < ApplicationController
   def create
+    @order_item = OrderItem.new(order_items_params)
+    
+    if @order_item.save
+      flash[:status] = :success
+      flash[:message] = "Added to cart!"
+      redirect_to product_path(id: params[:product_id])
+    else
+      flash.new[:status] = :error
+      flash.new[:message] = "Unable to add to cart :("
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def edit
