06985e00f54c2354df6e4a05b30d64b1b4c91cde
Jansen <v.jansen.martin@gmail.com>
2019-05-01 17:02:37 -0700

    Add conditional checking for existing merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,4 +1,18 @@
 class MerchantsController < ApplicationController
   def create
+    auth_hash = request.env["omniauth.auth"]
+    merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
+
+    if merchant
+      # Merchant was found in the database
+      flash[:status] = :success
+      flash[:message] = "Logged in as returning merchant #{merchant.name}"
+    else
+      # Merchant doesn't match anything in the DB
+      # TODO: Try to create a new merchant
+    end
+
+    session[:merchant_id] = merchant.id
+    redirect_to root_path
   end
 end
