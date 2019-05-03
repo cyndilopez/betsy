c00e00099fa42727c688b49478903418222d8d47
Maria Wissler <wmcarmelina@gmail.com>
2019-05-03 16:24:45 -0700

    started order_items test

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,9 +1,11 @@
 require "test_helper"
 
 describe OrderItem do
-  let(:order_item) { OrderItem.new }
+  let(:order_item) { order_items(:one) }
 
-  it "must be valid" do
-    value(order_item).must_be :valid?
+  describe "validations" do
+    it "is valid for product and order" do
+      expect(order_item.valid?).must_equal true
+    end
   end
 end
