d15541498c6023b60d7b618f1614fae5a9471a5f
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-03 13:46:53 -0700

    added category new and create methods. changed view to display categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,18 @@
 class CategoriesController < ApplicationController
-    
+  def new
+    @category = Category.new
+  end
+
+  def create
+    name = params[:name]
+    @category = Category.new(name: name)
+    @category.save
+    if @category
+      flash[:status] = :success
+      flash[:message] = "Saved category with id #{category.id} successfully"
+    else
+      flash[:status] = :error
+      flash[:message] = "Could not save the category"
+    end
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -67,6 +67,8 @@ class ProductsController < ApplicationController
     end
   end
 
+  private
+
   def product_params
     params.require(:product).permit(:name, :description, :price, :photoURL, :stock, :merchant_id)
   end
