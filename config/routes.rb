497603fb9971be908c9c8acd7e93d82997fd2ceb
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-03 13:51:06 -0700

    logout route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
+  delete "/logout", to: "merchants#destroy", as: "logout"
   resources :order_items, only: [:create, :edit, :update, :destroy]
   resources :merchants, only: [:index, :show]
 end
