c20d1edc0f5d506bd82b31554fb06dff8c19edbc
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-03 15:48:13 -0700

    added skip_before_action require_login for merchant, orderitem, and order controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
+  skip_before_action :require_login
 
   def create
     @order = Order.new
-    
   end
 
   private
-  
-    def order_params
-      params.require(:order).permit(:status)
-    end
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,7 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
+  skip_before_action :require_login
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
