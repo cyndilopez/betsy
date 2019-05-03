f362209383d2f3b15e959229348cf8429ec128a7
Amy Phung <phung.amy85@gmail.com>
2019-05-03 14:04:01 -0700

    change order Status from string to boolean data type

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_items
   
+  validates_associated :order_items
+  validates :status, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_000448) do
+ActiveRecord::Schema.define(version: 2019_05_03_205453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_000448) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "status"
+    t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
