ed7935764573f2e21d39b826e65004643d9a0084
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-04 15:23:33 -0700

    trying a nested route for category and product

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show, :new, :create, :edit, :update]
   resources :order_items, only: [:create, :edit, :update, :destroy]
   resources :categories, only: [:index, :new, :create]
+  resources :products do
+    resources :categories, only: [:create]
+  end
   resources :merchants, only: [:index, :show]
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
