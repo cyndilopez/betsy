bc70182c3aa733efd7aa6198e55a618a56e528a7
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-04 14:20:17 -0700

    link to browse by category in root, added categories#index to routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "products#root"
   resources :products, only: [:index, :show, :new, :create, :edit, :update]
   resources :order_items, only: [:create, :edit, :update, :destroy]
-  resources :categories, only: [:new, :create]
+  resources :categories, only: [:index, :new, :create]
   resources :merchants, only: [:index, :show]
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
