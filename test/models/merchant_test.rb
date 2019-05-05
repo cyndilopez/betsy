5a53bd798d34717473cb46ed1543198b7ab82214
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-04 18:57:16 -0700

    merchant validation and relation tests

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -2,8 +2,46 @@ require "test_helper"
 
 describe Merchant do
   let(:merchant) { Merchant.new }
+  let(:email) { "merchant@here.com" }
+  let(:username) { "new_merchant101" }
+  let(:valid_merchant) { merchants(:jenkins) }
 
-  it "must be valid" do
-    value(merchant).must_be :valid?
+  describe "validations" do
+    it "is valid for email and username restrictions" do
+      expect(valid_merchant.valid?).must_equal true
+    end
+
+    it "requires a username" do
+      merchant.email = email
+      expect(merchant.valid?).must_equal false
+      expect(merchant.errors).must_include "username"
+    end
+
+    it "requires the username to be unique" do
+      merchant_dup_username = valid_merchant.username
+      merchant_dup = Merchant.new(username: merchant_dup_username, email: email)
+      expect(merchant_dup.valid?).must_equal false
+      expect(merchant_dup.errors).must_include "username"
+    end
+
+    it "requires an email" do
+      merchant.username = username
+      expect(merchant.valid?).must_equal false
+      expect(merchant.errors).must_include "email"
+    end
+
+    it "requires a unique email" do
+      merchant_dup_email = valid_merchant.email
+      merchant_dup = Merchant.new(username: username, email: merchant_dup_email)
+      expect(merchant_dup.valid?).must_equal false
+      expect(merchant_dup.errors).must_include "email"
+    end
+  end
+
+  describe "relations" do
+    it "has many products" do
+      merchant = merchants(:bob)
+      expect(merchant.products.length).must_equal 2
+    end
   end
 end
