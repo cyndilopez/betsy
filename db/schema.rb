43a8ff0e515543aac77b61936eab23774d901c75
Maria Wissler <wmcarmelina@gmail.com>
2019-05-06 16:40:30 -0700

    migrate for cc to be bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_221620) do
+ActiveRecord::Schema.define(version: 2019_05_06_233755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_221620) do
     t.string "name"
     t.string "email"
     t.string "address"
-    t.integer "cc_num"
+    t.bigint "cc_num"
     t.integer "cc_expiration"
     t.string "zip_code"
     t.integer "cc_cvv"
