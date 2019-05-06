7241a8b0870b2142088ca36b118e7256cf91e7f0
Jansen <v.jansen.martin@gmail.com>
2019-05-06 16:04:10 -0700

    Add method to filter inactive products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,4 +28,9 @@ class ApplicationController < ActionController::Base
     merchant = Merchant.find_by(id: merchant_id)
     return merchant
   end
+
+  def select_active_products(products)
+    active_products = products.select { |p| p.active }
+    return active_products
+  end
 end
