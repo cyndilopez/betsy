e7b0965f08ff2091e246cf1474af1068c9a9eb97
Amy Phung <phung.amy85@gmail.com>
2019-05-06 13:39:31 -0700

    add orderitem create test

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -6,25 +6,53 @@ describe OrderItemsController do
     before do
     @product = products(:one)
     @order = orders(:one)
-  end
-    it "can create a new order item" do
-      
-      order_item_data = {
-        order_item: {
-          product_id: @product.id,
-          quantity: 2,
-          order_id: @order.id
-        }
+    
+    @order_item_data = {
+      order_item: {
+        product_id: @product.id,
+        quantity: 2,
+        order_id: @order.id
       }
-      
+    } 
+  end
+  
+    it "can create a new order item" do
       expect {
-        post order_items_path, params: order_item_data
+        post order_items_path, params: @order_item_data
       }.must_change "OrderItem.count", 1
       
       expect(session[:order_id]).wont_be_nil
       expect(flash[:status]).must_equal :success
       must_respond_with :found
       must_redirect_to product_path(id: @product.id)
+    end
+    
+    it "is connected to a product" do
+      order_item = OrderItem.new(product_id: @product.id, quantity: 1, order_id: @order.id)
+      expect(order_item.product).must_equal @product   
+    end
+    
+    it "has a quantity" do
+      order_item = OrderItem.new(product_id: @product.id, quantity: 1, order_id: @order.id)
+      expect(order_item.quantity).must_equal 1 
+    end
+    
+    it "will display an error if the quantity is more than is available in stock" do
+      product = products(:two)
+      order_item_data = {
+      order_item: {
+        product_id: product.id,
+        quantity: 5,
+        order_id: @order.id
+      }
+    } 
+    
+      expect{
+        post order_items_path, params: order_item_data
+      }.wont_change "OrderItem.count"
+      
+      expect(flash[:status]).must_equal :error
+      must_respond_with :redirect
     end
     
     it "is associated with an Order" do
