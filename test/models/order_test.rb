d8e7bc58a5021e08f11f346f3d4c9907ef14f374
Maria Wissler <wmcarmelina@gmail.com>
2019-05-07 15:26:17 -0700

    added regex validations and test model passing

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -18,6 +18,16 @@ describe Order do
       expect(order.valid?).must_equal false
     end
 
+    it "is invalid without an email on correct format" do
+      order.email = "jansenada.edu"
+      expect(order.valid?).must_equal false
+    end
+
+    it "is valid with an email in the right format" do
+      order.email = "customer@email.com"
+      expect(order.valid?).must_equal true
+    end
+
     it "is invalid without an address" do
       order.address = nil
       expect(order.valid?).must_equal false
@@ -28,9 +38,29 @@ describe Order do
       expect(order.valid?).must_equal false
     end
 
+    it "is invalid without a cc_num in the right format" do
+      order.cc_num = "ASj34JKJ@#$45"
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without a cc_num in the right format" do
+      order.cc_num = "1111222233334444"
+      expect(order.valid?).must_equal true
+    end
+
     it "is invalid without a cc_cvv" do
       order.cc_cvv = nil
       expect(order.valid?).must_equal false
+    end
+
+    it "is invalid for an incorrect input of cc_cvv" do
+      order.cc_cvv = "ABC!2"
+      expect(order.valid?).must_equal false
+    end
+
+    it "is valid for an correct input of cc_cvv" do
+      order.cc_cvv = "123"
+      expect(order.valid?).must_equal true
     end
 
     it "is invalid without a cc_expiration" do
@@ -38,9 +68,14 @@ describe Order do
       expect(order.valid?).must_equal false
     end
 
-    it "is invalid without a cc_num" do
-      order.cc_num = nil
+    it "is invalid without a valid input for cc_expiration" do
+      order.cc_expiration = "Jan2019"
       expect(order.valid?).must_equal false
+    end
+
+    it "is valid with a valid cc_expiration" do
+      order.cc_expiration = "102024"
+      expect(order.valid?).must_equal true
     end
 
     it "is invalid without a zip_code" do
@@ -48,9 +83,28 @@ describe Order do
       expect(order.valid?).must_equal false
     end
 
+    it "is invalid without a zip_code in the right format" do
+      order.zip_code = "ZIP1234"
+      expect(order.valid?).must_equal false
+    end
+
+    it "is valid with a correct zip_code" do
+      order.zip_code = "98103"
+      expect(order.valid?).must_equal true
+    end
+
     it "is invalid without a valid status" do
       order.status = "cancelled"
       expect(order.valid?).must_equal false
     end
+
+    # describe "relations" do
+    #   it "has order items" do
+    #     order_item = order.order_items(:starbursts)
+    #     expect(order.order_item.quantity).must_equal 1
+
+    #     category = categories(:confection)
+    #     expect(category.products.length).must_equal 1
+    #   end
   end
 end
