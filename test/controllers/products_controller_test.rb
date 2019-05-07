0584c7c7b21d4e8854220259e6088568faf238f5
Jansen <v.jansen.martin@gmail.com>
2019-05-07 07:53:06 -0700

    Revise first edit action test to include a check for authorized user

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -104,10 +104,13 @@ describe ProductsController do
 
     describe "edit" do
       before do
-        @product = Product.first
+        @product = merchants(:jenkins).products.first
         perform_login(merchants(:jenkins))
       end
-      it "responds with OK for a real product" do
+      it "responds with OK for a real product and an authorized merchant" do
+        expect(session[:merchant_id]).must_equal merchants(:jenkins).id
+        expect(@product.merchant_id).must_equal merchants(:jenkins).id
+
         get edit_product_path(@product)
 
         must_respond_with :ok
