a535236e0cf19878c415e9e47fe4ebcbcd285fae
Maria Wissler <wmcarmelina@gmail.com>
2019-05-06 22:21:09 -0700

    test for order validation form now passing, cleaned the test

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -2,19 +2,54 @@ require "test_helper"
 
 describe Order do
   let(:order) { orders(:two) }
-  let(:order_noname) { orders(:no_name) }
 
   describe "validations" do
     it "is valid for name, status, email, cc_num, cc_cvv, cc_expiration and zip code" do
       expect(order.valid?).must_equal true
     end
 
-    it "requires a name" do
-      order = Order.new()
-    end
-
     it "is invalid without a name" do
       order.name = nil
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without an email" do
+      order.email = nil
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without an address" do
+      order.address = nil
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without a cc_num" do
+      order.cc_num = nil
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without a cc_cvv" do
+      order.cc_cvv = nil
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without a cc_expiration" do
+      order.cc_expiration = nil
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without a cc_num" do
+      order.cc_num = nil
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without a zip_code" do
+      order.zip_code = nil
+      expect(order.valid?).must_equal false
+    end
+
+    it "is invalid without a valid status" do
+      order.status = "cancelled"
       expect(order.valid?).must_equal false
     end
   end
