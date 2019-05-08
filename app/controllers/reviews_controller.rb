c7601c137bdfa664a077b65ec2ec689620762462
Maria Wissler <wmcarmelina@gmail.com>
2019-05-07 23:28:01 -0700

    added review controller method for new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+  def new
+    @product = Product.find_by(id: params[:product_id])
+    if !@merchant.nil? && @merchant.id == @product.merchant.id
+      flash[:error] = "You can't review your own product!"
+      redirect_to product_path(@product.id)
+    else
+      render_404 unless @product
+      @review = Review.new
+    end
+  end
+
+  def review_params
+    params.permit(:rating, :comment, :product_id)
+  end
 end
