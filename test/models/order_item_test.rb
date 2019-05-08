2727948d86c9365fa0f9d47e4f5039146ab98be2
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-08 14:31:51 -0700

    quantity validation tests for quantity and requiring product and order

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -4,33 +4,51 @@ describe OrderItem do
   let(:order_item) { order_items(:one) }
 
   describe "validations" do
-    it "is valid for product and order" do
+    it "is valid for product, order and quantity" do
       expect(order_item.valid?).must_equal true
+    end
+
+    it "requires an order" do
+      order_item.order_id = nil
+      expect(order_item.valid?).must_equal false
+    end
+
+    it "requires a product" do
+      order_item.product_id = nil
+      expect(order_item.valid?).must_equal false
+    end
+
+    it "has a quantity that is an integer and greater than zero" do
+      cases = ["string", 1.45, :tree, -1, -1.45]
+      cases.each do |item|
+        order_item.quantity = item
+        expect(order_item.valid?).must_equal false
+      end
     end
 
     it "has a product price" do
-      order_item.unit_price = "10.99"
+      order_item.product.price = "10.99"
       expect(order_item.valid?).must_equal true
     end
+  end
 
-    
+  describe "relations" do
     it "belongs to a product" do
-      expect(order_item.product).wont_be_nil 
+      expect(order_item.product).wont_be_nil
       expect(order_item.product).must_be_instance_of Product
     end
-    
+
     it "belongs to an order" do
       expect(order_item.order).wont_be_nil
       expect(order_item.order).must_be_instance_of Order
     end
-    
+
     it "has a merchant" do
       expect(order_item.merchant).wont_be_nil
       expect(order_item.merchant).must_be_instance_of Merchant
     end
-    
   end
-  
+
   describe "subtotal" do
     it "calculates a subtotal" do
       product = products(:starbursts)
@@ -38,9 +56,8 @@ describe OrderItem do
       price = product.price
       order_item = OrderItem.new(product: product, order: order, quantity: 5)
       total = price * 5
-      
+
       expect(order_item.subtotal).must_equal total
     end
   end
-
 end
