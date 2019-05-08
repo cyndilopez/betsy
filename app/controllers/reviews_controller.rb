0d9ba7fdf9117692bf2d6b617bc815c7795d91c5
Maria Wissler <wmcarmelina@gmail.com>
2019-05-07 23:31:37 -0700

    added review controller method for create, migrations pending is proposal

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,24 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def create
+    @product = Product.find_by(id: params[:product_id])
+    if !@merchant.nil? && @merchant.id == @product.merchant.id
+      flash.now[:error] = "You can't review your own product!"
+      redirect_to product_path(@product.id)
+    else
+      @review = Review.new(review_params)
+      if @review.save
+        flash[:success] = "Your review was added succesfully!"
+        redirect_to product_path(@review.product_id)
+      else
+        flash[:error] = @review.errors
+        @product = Product.find_by(id: params[:product_id])
+        render :new, status: :bad_request
+      end
+    end
+  end
+
   def review_params
     params.permit(:rating, :comment, :product_id)
   end
