1c6879b677bc4da86ee9ceb933259e82741c7740
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-08 15:28:44 -0700

    review controller create edits

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,13 +5,18 @@ class ReviewsController < ApplicationController
       flash[:error] = "You can't review your own product!"
       redirect_to product_path(@product.id)
     else
-      render_404 unless @product
+      unless @product
+        head :not_found 
+      end
       @review = Review.new
     end
   end
 
   def create
     @product = Product.find_by(id: params[:product_id])
+    unless @product
+      head :not_found
+    end
     if !@merchant.nil? && @merchant.id == @product.merchant.id
       flash.now[:error] = "You can't review your own product!"
       redirect_to product_path(@product.id)
@@ -21,8 +26,8 @@ class ReviewsController < ApplicationController
         flash[:success] = "Your review was added succesfully!"
         redirect_to product_path(@review.product_id)
       else
-        flash[:error] = @review.errors
-        @product = Product.find_by(id: params[:product_id])
+        flash[:status] = :error
+        flash[:message] = "Couldn't save review: #{@review.errors.messages}"
         render :new, status: :bad_request
       end
     end
